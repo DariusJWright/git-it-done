178c03e949358da68217598f76cd997b63a36356
darshnablah <dwright758@gmail.com>
2020-06-25 18:11:48 -0500

    added language buttons/capability

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -6,6 +6,8 @@ var nameInput = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 // variable linking to <span> element that displays search info
 var repoSearchTerm = document.querySelector("#repo-search-term");
+// variable linking to language buttons
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -97,4 +99,32 @@ var displayRepos = function (repos, searchTerm) {
     }
 };
 
+var getFeaturedRepos = function (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response){
+        if (response.ok) {
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+        }
+        else {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
+
+var buttonClickHandler = function (event) {
+    var language = event.target.getAttribute("data-language");
+    console.log(language);
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
